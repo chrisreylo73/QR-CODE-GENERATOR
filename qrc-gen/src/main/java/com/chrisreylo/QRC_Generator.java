dbece8bb889f1a832949849dbe2219edee9a68e4
chrisreylo73 <chrisreylo73@gmail.com>
2023-10-22 13:15:27 -0600

    Added plugin

--- qrc-gen/src/main/java/com/chrisreylo/QRC_Generator.java
+++ qrc-gen/src/main/java/com/chrisreylo/QRC_Generator.java
@@ -1,5 +1,13 @@
 package com.chrisreylo;
 
+// import com.google.zxing.BarcodeFormat;
+// import com.google.zxing.MultiFormatWriter;
+// import com.google.zxing.WriterException;
+// import com.google.zxing.client.j2se.MatrixToImageWriter;
+// import com.google.zxing.common.BitMatrix;
+// import java.io.IOException;
+// import java.nio.file.Paths;
+
 public class QRC_Generator {
 
   QRC_Generator() {
