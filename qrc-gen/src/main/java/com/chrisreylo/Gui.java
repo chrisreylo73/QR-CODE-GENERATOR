6d0ba5867a97b617adfbe655b59cb5bbc324df0c
chrisreylo73 <chrisreylo73@gmail.com>
2023-10-24 00:11:05 -0600

    Deleted some unused imports

--- qrc-gen/src/main/java/com/chrisreylo/Gui.java
+++ qrc-gen/src/main/java/com/chrisreylo/Gui.java
@@ -4,10 +4,7 @@ import com.google.zxing.WriterException;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.io.IOException;
-import java.util.*;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 
