4b89bc1a6af4104b462bd678f1a7945a348c2b76
chrisreylo73 <chrisreylo73@gmail.com>
2023-10-24 00:12:34 -0600

    Added comments

--- qrc-gen/src/main/java/com/chrisreylo/QRCodeGenerator.java
+++ qrc-gen/src/main/java/com/chrisreylo/QRCodeGenerator.java
@@ -15,10 +15,13 @@ public class QRCodeGenerator {
 
   public Gui gui;
 
+  // Constructor for the QRCodeGenerator class
   QRCodeGenerator() throws WriterException, IOException {
+    // Initialize the GUI
     gui = new Gui(this);
   }
 
+  // Method to read a PDF file and return its contents as a byte array
   private static byte[] readPDF(String filePath) throws IOException {
     File file = new File(filePath);
     byte[] data = new byte[(int) file.length()];
@@ -30,23 +33,31 @@ public class QRCodeGenerator {
     return data;
   }
 
+  // Method to generate a QR code based on input data
   public void gen(String data, String path, String name, String format)
     throws WriterException, IOException {
     if (data.contains(".pdf")) {
+      // If the input data is a PDF file, read its contents and encode as Base64
       byte[] pdfData = readPDF(data);
       data = Base64.getEncoder().encodeToString(pdfData);
     }
 
     try {
+      // Generate a QR code from the input data using the ZXing library
       BitMatrix matrix = new MultiFormatWriter()
         .encode(data, BarcodeFormat.QR_CODE, 500, 500);
+
+      // Write the QR code as an image to the specified path and format
       MatrixToImageWriter.writeToPath(
         matrix,
         format,
         Paths.get(path + "/" + name + "." + format)
       );
+
+      // Print a success message to the console
       System.out.println("\n" + name + "." + format + " Generated!");
     } catch (Exception e) {
+      // Print an error message if an exception occurs during QR code generation
       System.out.println("ERROR OCCURRED: " + e);
     }
   }
